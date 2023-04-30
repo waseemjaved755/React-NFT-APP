353ee5e7e8f90afb644bd8efc5a800064bf3800d
Muhammad Waseem Javed <waseemjaved755@gmail.com>
2023-04-30 18:11:50 +0500

    mycommit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,25 @@
-function ListGroup ()
-{
-    return <h1> List Group</h1>
+import { Fragment } from "react";
+
+function ListGroup() {
+  let Items = ["Lahore", "Karachi", "Multan"];
+
+  return (
+    <>
+      <h1> List </h1>
+      {Items.length === 0 ? <p>No item found</p> : null}
+      <ul className="list-group">
+        {Items.map((Items, index) => (
+          <li
+            className="list-group-items"
+            key={Items}
+            onClick={() => console.log(Items, index)}
+          >
+            {Items}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
